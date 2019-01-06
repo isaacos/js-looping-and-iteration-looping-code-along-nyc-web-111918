acee7e7d17722202f69a9a6b8fc03f7eb943327b
isaac <oseasisaac91@gmail.com>
2019-01-06 11:54:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function printBadges(namesArray){
+  for (let i = 0; i < namesArray.length; i++) {
+      console.log(`Welcome ${namesArray[i]}! You are employee #${i + 1}.`)
+  }
+  return namesArray
+}
+
+function tailsNeverFails(){
+  let tails_count = 0
+
+  while(Math.random() >= 0.5){
+    ++tails_count
+  }
+  return `You got ${tails_count} tails in a row!`
+}
